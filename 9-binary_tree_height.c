11f095dc45936371115646925001b6d750749127
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-08-04 08:30:29 +0000

    update 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -22,8 +22,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	left = binary_tree_height(tree->left);
 	right = binary_tree_height(tree->right);
 
-	if (left > right) /* return the max height found plus 1 for root node */
-		return (left + 1);
+	if (left > right) /* return the max height found */
+		return (left);
 	else
-		return (right + 1);
+		return (right);
 }
