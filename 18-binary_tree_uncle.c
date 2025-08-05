7f82112c4e9e317e021d89bb0acaaed6232a5f92
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-08-05 07:13:15 +0000

    update 18-binary_tree_uncle.c

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -16,17 +16,14 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	binary_tree_t *parent; /* to store node's parent */
 	binary_tree_t *uncle; /* to store uncle node */
 
-	if (node == NULL || node->parent == NULL)
+	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 		return (NULL);
 
 	/* find node's parent */
 	parent = node->parent;
 
-	/* move node to node's parent */
-	node = node->parent;
-
 	/* move node to node's grandparent */
-	node = node->parent;
+	node = node->parent->parent;
 
 	/* find and return node's uncle */
 	if (node->left == parent)
