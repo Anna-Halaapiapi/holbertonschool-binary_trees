1aea7a715ed98a0d86942a279cfc19bdbb9a83ca
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-08-05 03:02:40 +0000

    Save progress

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+#include <stdio.h>
+#include <stdlib.h> /* for malloc, NULL */
+#include <stddef.h> /* for size_t */
+
+/**
+ * binary_tree_is_perfect - checks if binary tree is perfect
+ * @tree: pointer to root node of tree
+ *
+ * Return: 0 (false or tree is NULL) or 1 (true)
+ */
+
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	
+
+}
